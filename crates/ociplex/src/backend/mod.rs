6f031e705866e9bb519a635693e4c18eb507bfe0
Christophe de Dinechin <dinechin@redhat.com>
2023-03-03 11:40:18 +0100

    Add CommonCmd commands to the back-end
    The CommonCmd commands are all unimplemented by default. Backends can implement
    them as required.
    
    Signed-off-by: Christophe de Dinechin <dinechin@redhat.com>

--- crates/ociplex/src/backend/mod.rs
+++ crates/ociplex/src/backend/mod.rs
@@ -2,7 +2,7 @@ use std::fmt::Debug;
 
 use anyhow::{anyhow, Result};
 
-use liboci_cli::{GlobalOpts, StandardCmd};
+use liboci_cli::{CommonCmd, GlobalOpts, StandardCmd};
 
 mod cli;
 mod trivial;
@@ -24,16 +24,6 @@ impl Config {
 }
 
 pub trait Backend: Debug {
-    fn create(&self, args: liboci_cli::Create) -> Result<()>;
-    fn start(&self, args: liboci_cli::Start) -> Result<()>;
-    fn kill(&self, args: liboci_cli::Kill) -> Result<()>;
-    fn delete(&self, args: liboci_cli::Delete) -> Result<()>;
-    fn state(&self, args: liboci_cli::State) -> Result<()>;
-
-    fn exec(&self, args: liboci_cli::Exec) -> Result<()> {
-        Err(anyhow!("exec subcommand unimplemented: {:?}", args))
-    }
-
     fn standard_command(&self, cmd: liboci_cli::StandardCmd) -> Result<()> {
         match cmd {
             StandardCmd::Create(args) => self.create(args),
@@ -42,5 +32,59 @@ pub trait Backend: Debug {
             StandardCmd::Delete(args) => self.delete(args),
             StandardCmd::State(args) => self.state(args),
         }
+    }
+
+    fn common_command(&self, cmd: liboci_cli::CommonCmd) -> Result<()> {
+        match cmd {
+            CommonCmd::Checkpointt(args) => self.checkpoint(args),
+            CommonCmd::Events(args) => self.events(args),
+            CommonCmd::Exec(args) => self.exec(args),
+            CommonCmd::List(args) => self.list(args),
+            CommonCmd::Pause(args) => self.pause(args),
+            CommonCmd::Ps(args) => self.ps(args),
+            CommonCmd::Resume(args) => self.resume(args),
+            CommonCmd::Run(args) => self.run(args),
+            CommonCmd::Update(args) => self.update(args),
+            CommonCmd::Spec(args) => self.spec(args),
+        }
+    }
+
+    // StandardCmd in liboci-cli
+    fn create(&self, args: liboci_cli::Create) -> Result<()>;
+    fn start(&self, args: liboci_cli::Start) -> Result<()>;
+    fn kill(&self, args: liboci_cli::Kill) -> Result<()>;
+    fn delete(&self, args: liboci_cli::Delete) -> Result<()>;
+    fn state(&self, args: liboci_cli::State) -> Result<()>;
+
+    // CommonCmd in liboci-cli are not implemented by default
+    fn checkpoint(&self, args: liboci_cli::Checkpoint) -> Result<()> {
+        Err(anyhow!("checkpoint subcommand unimplemented: {:?}", args))
+    }
+    fn events(&self, args: liboci_cli::Events) -> Result<()> {
+        Err(anyhow!("events subcommand unimplemented: {:?}", args))
+    }
+    fn exec(&self, args: liboci_cli::Exec) -> Result<()> {
+        Err(anyhow!("exec subcommand unimplemented: {:?}", args))
+    }
+    fn list(&self, args: liboci_cli::List) -> Result<()> {
+        Err(anyhow!("list subcommand unimplemented: {:?}", args))
+    }
+    fn pause(&self, args: liboci_cli::Pause) -> Result<()> {
+        Err(anyhow!("pause subcommand unimplemented: {:?}", args))
+    }
+    fn ps(&self, args: liboci_cli::Ps) -> Result<()> {
+        Err(anyhow!("ps subcommand unimplemented: {:?}", args))
+    }
+    fn resume(&self, args: liboci_cli::Resume) -> Result<()> {
+        Err(anyhow!("resume subcommand unimplemented: {:?}", args))
+    }
+    fn run(&self, args: liboci_cli::Run) -> Result<()> {
+        Err(anyhow!("run subcommand unimplemented: {:?}", args))
+    }
+    fn update(&self, args: liboci_cli::Update) -> Result<()> {
+        Err(anyhow!("update subcommand unimplemented: {:?}", args))
+    }
+    fn spec(&self, args: liboci_cli::Spec) -> Result<()> {
+        Err(anyhow!("spec subcommand unimplemented: {:?}", args))
     }
 }
