e3a54a045d66dce8ecffd8f4990fbea7fd320f14
Christophe de Dinechin <dinechin@redhat.com>
2023-04-22 03:34:34 +0200

    ociplex: More complete checkpoint implementation
    This passes the various options using protobuf JSON encoding.
    
    Signed-off-by: Christophe de Dinechin <dinechin@redhat.com>

--- crates/ociplex/src/backend/shimv2.rs
+++ crates/ociplex/src/backend/shimv2.rs
@@ -1,6 +1,13 @@
 use containerd_shim_protos as shim;
 
 use containerd_shim_protos as client;
+use protobuf::{
+    well_known_types::{
+        any::Any,
+        struct_::{value::Kind, Struct, Value},
+    },
+    MessageField,
+};
 use shim::ttrpc::context::Context;
 use shim::{api, api::ConnectResponse, Client, TaskClient};
 
@@ -41,7 +48,7 @@ struct ShimV2Backend {
     global_opts: GlobalOpts,
 }
 
-fn path_buf_to_string<'a>(kind: &str, path: &'a Path) -> Result<&'a str> {
+fn path_buf_to_str<'a>(kind: &str, path: &'a Path) -> Result<&'a str> {
     path.to_str().ok_or_else(|| {
         anyhow!(
             "ShimV2 {} path {} contains invalid characters",
@@ -49,6 +56,57 @@ fn path_buf_to_string<'a>(kind: &str, path: &'a Path) -> Result<&'a str> {
             path.display(),
         )
     })
+}
+
+fn path_buf_to_value(kind: &str, path: &Path) -> Result<Value> {
+    let string = path_buf_to_str(kind, path)?;
+    Ok(Value {
+        kind: Some(Kind::StringValue(string.to_string())),
+        ..Default::default()
+    })
+}
+
+fn string_value(value: &str) -> Value {
+    Value {
+        kind: Some(Kind::StringValue(value.into())),
+        ..Default::default()
+    }
+}
+
+fn bool_value(value: bool) -> Value {
+    Value {
+        kind: Some(Kind::BoolValue(value)),
+        ..Default::default()
+    }
+}
+
+fn u32_value(value: u32) -> Value {
+    Value {
+        kind: Some(Kind::NumberValue(f64::from(value))),
+        ..Default::default()
+    }
+}
+
+fn add_option(opts: &mut Struct, kind: &str, value: Value) {
+    opts.fields.insert(kind.to_string(), value);
+}
+
+fn add_u32_option(opts: &mut Struct, kind: &str, value: u32) {
+    add_option(opts, kind, u32_value(value));
+}
+
+fn add_string_option(opts: &mut Struct, kind: &str, value: &str) {
+    add_option(opts, kind, string_value(value));
+}
+
+fn add_path_option(opts: &mut Struct, kind: &str, path: &Path) -> Result<()> {
+    let path = path_buf_to_value(kind, path)?;
+    add_option(opts, kind, path);
+    Ok(())
+}
+
+fn add_bool_option(opts: &mut Struct, kind: &str, value: bool) {
+    add_option(opts, kind, bool_value(value));
 }
 
 impl ShimV2Backend {
@@ -89,7 +147,7 @@ impl ShimV2Backend {
     }
 
     fn invoke(&self, pid: &str) -> Result<(TaskClient, Context, ConnectResponse)> {
-        let socket_path = path_buf_to_string("socket", &self.socket)?;
+        let socket_path = path_buf_to_str("socket", &self.socket)?;
         let client = client::Client::connect(socket_path).or_else(|_| self.launch(socket_path))?;
         let task_client = client::TaskClient::new(client);
         let context = Context::default();
@@ -106,7 +164,7 @@ impl Backend for ShimV2Backend {
     // Standard commands (from liboci_cli::StandardCmd)
     fn create(&self, args: liboci_cli::Create) -> Result<()> {
         let (task, context, connect_response) = self.invoke(&args.container_id)?;
-        let bundle = path_buf_to_string("bundle", &args.bundle)?;
+        let bundle = path_buf_to_str("bundle", &args.bundle)?;
 
         if let Some(socket) = args.console_socket {
             println!(
@@ -206,15 +264,59 @@ impl Backend for ShimV2Backend {
     // Common non-standard commands (from liboci_cli::CommonCmd)
     fn checkpoint(&self, args: liboci_cli::Checkpoint) -> Result<()> {
         let (task, context, connect_response) = self.invoke(&args.container_id)?;
-        let image_path = path_buf_to_string("image_path", &args.image_path)?;
+        let image_path = path_buf_to_str("image_path", &args.image_path)?;
+        let mut opts = Struct::new();
+
+        if let Some(work_path) = args.work_path {
+            add_path_option(&mut opts, "work-path", &work_path);
+        }
+        if let Some(parent_path) = args.parent_path {
+            add_path_option(&mut opts, "parent-path", &parent_path);
+        }
+        if args.leave_running {
+            add_bool_option(&mut opts, "leave-running", args.leave_running);
+        }
+        if args.tcp_established {
+            add_bool_option(&mut opts, "tcp-established", args.tcp_established);
+        }
+        if args.ext_unix_sk {
+            add_bool_option(&mut opts, "ext-unix-sk", args.ext_unix_sk);
+        }
+        if args.shell_job {
+            add_bool_option(&mut opts, "shell-job", args.shell_job);
+        }
+        if args.lazy_pages {
+            add_bool_option(&mut opts, "lazy-pages", args.lazy_pages);
+        }
+        if let Some(status_fd) = args.status_fd {
+            add_u32_option(&mut opts, "status-fd", status_fd);
+        }
+        if let Some(page_server) = args.page_server {
+            add_string_option(&mut opts, "page-server", &page_server);
+        }
+        if args.file_locks {
+            add_bool_option(&mut opts, "file-locks", args.file_locks);
+        }
+        if args.pre_dump {
+            add_bool_option(&mut opts, "pre-dump", args.pre_dump);
+        }
+        if let Some(manage_cgroups_mode) = args.manage_cgroups_mode {
+            add_string_option(&mut opts, "manage-cgroups-mode", &manage_cgroups_mode);
+        }
+        if args.empty_ns {
+            add_bool_option(&mut opts, "empty-ns", args.empty_ns);
+        }
+        if args.auto_dedup {
+            add_bool_option(&mut opts, "auto-dedup", args.auto_dedup);
+        }
+
+        let options = Any::pack(&opts)?;
         let req = api::CheckpointTaskRequest {
             id: args.container_id,
             path: image_path.to_owned(),
+            options: MessageField::some(options),
             ..Default::default()
         };
-
-        // REVISIT: There are plenty of ignored options for "checkpoint"
-        // They should probably be parsed and put in 'options'
 
         let resp = task.checkpoint(context, &req)?;
         if self.global_opts.debug {
